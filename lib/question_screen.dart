fc443bf57dbf69a1f367cf60a59f8fc0fbb23450
Khanh <21520982@ms.uit.edu.vn>
2023-04-19 08:29:46 +0700

    checkpoint section-3.68: "styled_button"

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,4 +1,6 @@
+import 'package:adv_basics/styled_button.dart';
 import 'package:flutter/material.dart';
+import './styled_button.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -11,8 +13,31 @@ class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(color: Colors.amber),
-      child: const Center(child: const Text("Hello")),
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+        Color.fromARGB(255, 161, 218, 207),
+        Color.fromARGB(255, 65, 255, 252)
+      ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+      child: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          const Text("Question 1..."),
+          const SizedBox(
+            height: 30,
+          ),
+          // ElevatedButton(onPressed: () {}, child: const Text("Answer 1")),
+          // ElevatedButton(onPressed: () {}, child: const Text("Answer 2")),
+          // ElevatedButton(onPressed: () {}, child: const Text("Answer 3")),
+          StyledButton("Answer 1", () {
+            print("1");
+          }),
+          StyledButton("Answer 2", () {
+            print("2");
+          }),
+          StyledButton("Answer 3", () {
+            print("3");
+          }),
+        ]),
+      ),
     );
   }
 }
